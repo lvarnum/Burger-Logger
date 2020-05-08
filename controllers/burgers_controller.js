a63d7e36e183b814e14a3f5d506b770ca667d12c
lvarnum <lvarnum@email.arizona.edu>
2020-05-08 16:25:07 -0700

    Burgers displaying

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,14 @@ const burger = require("../models/burger.js");
 const router = express.Router();
 
 // ---------- Create Router Paths ----------
+router.get("/", (req, res) => {
+    burger.selectAll(function (data) {
+        res.render("index", {burgers: data});
+    });
+});
 
+router.post("/api/burgers", (req, res) => {
 
+})
 // ---------- Export Router ----------
 module.exports = router;
