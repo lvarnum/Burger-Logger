18ed98e0cc2462d6b018f9cdbbf7835d8987782a
lvarnum <lvarnum@email.arizona.edu>
2020-05-08 14:30:11 -0700

    Starter code finished

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,12 @@
+// ---------- Dependencies ----------
+const express = require("express");
+const burger = require("../models/burger.js");
+
+// ---------- Create Router ----------
+const router = express.Router();
+
+// ---------- Create Router Paths ----------
+
+
+// ---------- Export Router ----------
+module.exports = router;
--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 // ---------- Dependencies ----------
 const express = require("express");
+
+// ---------- Port ----------
+var PORT = process.env.PORT || 3000;
+
+// ---------- App Set-Up ----------
+const app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ---------- Express-Handlebars Set-UP ----------
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// ---------- Import Routes ----------
+const routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+
+// ---------- Start Server ----------
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
